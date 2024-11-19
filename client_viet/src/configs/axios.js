5219c1bfffe46da27fda51b7f10f685c7091241f
thinh <thinh.ngohoai@gmail.com>
2024-11-19 23:10:41 +0700

    add interceptors 's axios, implement jwt

--- client_viet/src/configs/axios.js
+++ client_viet/src/configs/axios.js
@@ -1,6 +1,15 @@
 import axios from "axios";
 
 
-export default axios.create({
+ const instance = axios.create({
     baseURL: "http://localhost:8080/api/",
 })
+instance.interceptors.request.use((config) =>{
+    const token = localStorage.getItem("jwt");
+    if (token) {
+        instance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+})
+export default instance;
+
