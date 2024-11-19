d9a1ed2c4eadea3df79d762e0eb03a778135a040
thinh <thinh.ngohoai@gmail.com>
2024-11-19 23:20:34 +0700

    add constrant

---
+++ client_viet/src/configs/data.js
@@ -0,0 +1,6 @@
+
+
+export const city = {
+     province_id:'79',
+    province_name:'Thành phố Hồ Chí Minh'
+}
---
+++ client_viet/src/utils/PaymentMethod.js
@@ -0,0 +1,16 @@
+const method = [
+    {
+        id: 'ONLINE',
+        title: 'Thanh toán bằng chuyển khoản'
+    },
+    {
+        id: 'OFFLINE',
+        title: 'Thanh toán trực tiếp tại cửa hàng'
+    },
+    {
+        id: 'COD',
+        title: 'Thanh toán tại điểm giao hàng'
+    }
+];
+
+export default method;
