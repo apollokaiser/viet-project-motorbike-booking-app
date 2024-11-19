6ef51b96aa708a849c7040f4c8120d0ea3cce13b
thinh <thinh.ngohoai@gmail.com>
2024-11-19 23:03:17 +0700

    add material UI

--- client_viet/src/main.jsx
+++ client_viet/src/main.jsx
@@ -1,14 +1,17 @@
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { createRoot } from "react-dom/client";
 import Router from "./router/Router";
-import { Provider } from 'react-redux'
+import { Provider } from "react-redux";
 import store from "./redux/store";
+import { StyledEngineProvider } from "@mui/material/styles";
 const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
 
 createRoot(document.getElementById("root")).render(
   <GoogleOAuthProvider clientId={CLIENT_ID}>
-    <Provider store={store}>
-      <Router/>
-    </Provider>
+    <StyledEngineProvider injectFirst>
+      <Provider store={store}>
+        <Router />
+      </Provider>
+    </StyledEngineProvider>
   </GoogleOAuthProvider>
 );
