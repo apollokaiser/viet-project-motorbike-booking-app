1878dcb0148d4e18a2bdeeda885fff124dd0337b
thinh <thinh.ngohoai@gmail.com>
2024-11-19 23:31:36 +0700

    update utils

--- QLthuexeBackend/src/utils/initialize.js
+++ QLthuexeBackend/src/utils/initialize.js
@@ -1,5 +1,6 @@
 import hangXe from "../models/hangxe.js";
 import loaiXe from "../models/loaixe.js";
+import vanChuyen from "../models/vanchuyen.js";
 const brandsData = [
     {
         ma_hang: "HONDA",
@@ -24,14 +25,24 @@ const categoriesData = [
         ten_loai: "Xe số"
     }
 ]
+const phiVanChuyenData = [
+    {
+        ma_phi: "internal",
+        ma_quan: '778',
+        ma_tinh: '79',
+        phi_van_chuyen: 0
+    },
+    {
+        ma_phi: "external",
+        phi_van_chuyen: 50000
+    }
+]
 
 export const addLoaiXes = async () => {
     try {
         const count = await loaiXe.count();
         if (count == 0) {
-            categoriesData.map(category=>{
-                loaiXe.create(category);
-            })
+            loaiXe.bulkCreate(categoriesData);
         }
     } catch (error) {
         console.log("Initialize - CATEGORY failed: " + error);
@@ -41,31 +52,47 @@ export const addHangXes = async () => {
     try {
         const count = await hangXe.count();
         if (count == 0) {
-            brandsData.map(brand=>{
-                hangXe.create(brand);
-            })
+            hangXe.create(brandsData);
         }
     } catch (error) {
         console.log("Initialize - BRAND failed: " + error)
     }
 }
+export const addPhiVanChuyen = async () => {
+    try {
+        const count = await vanChuyen.count();
+        if (count == 0) {
+            vanChuyen.bulkCreate(phiVanChuyenData);
+        }
+    } catch (error) {
+        console.log("Initialize - PHI VAN CHUYEN failed: " + error)
+    }
+}
 
 
 export const orderStatus = {
-    PENDING:{
-        id:1,
-        name:'Đang chờ'
+    PENDING_PAYMENT: {
+        id: 0,
+        name: 'Chờ thanh toán'
     },
-    DELIVERED:{
-        id:2,
-        name:'Đã giao'
+    WAITING_CONFIRMATION: {
+        id: 1,
+        name: 'Chờ xác nhận'
     },
-    RETURNED:{
-        id:3,
-        name:'Đã trả'
+    CONFIRMED: {
+        id: 2,
+        name: 'Đã xác nhận'
     },
-    CANCELLED:{
-        id:4,
-        name:'Đã hủy'
+    DELIVERED: {
+        id: 3,
+        name: 'Đã giao'
+    },
+    RETURNED: {
+        id: 3,
+        name: 'Đã trả'
+    },
+    CANCELLED: {
+        id: 4,
+        name: 'Đã hủy'
     }
 }
--- QLthuexeBackend/src/utils/utils.js
+++ QLthuexeBackend/src/utils/utils.js
@@ -1,5 +1,7 @@
-import jwt from"jsonwebtoken";
-const secretKey = "myapp";
+import jwt from "jsonwebtoken";
+import QueryString from "qs";
+import crypto from "crypto";
+const secretKey = "ce851b9793332b8bfb999ce5338dfe75270e769639eea987b99e63c5325239ef";
 const secretRefreshToken = "myappsecret";
 
 class Utils {
@@ -8,6 +10,7 @@ class Utils {
             ...user
         },
             secretKey,
+            { algorithm: 'HS256' },
             { expiresIn: '1h' });
     }
     /**
@@ -23,15 +26,14 @@ class Utils {
     }
 
     static getDecodeTokenData(token) {
-        let decoded_data = null;
-        jwt.verify(token, secretKey, (err, decoded) => {
-            if (err) {
-                console.log('token err', err);
-                return null;
-            }
-            decoded_data = decoded;
-        });
-        return decoded_data;
+        try {
+            token = JSON.parse(token);
+            const decodedData = jwt.verify(token, secretKey, { algorithms: ['HS256'] });
+            return decodedData;
+        } catch (error) {
+            console.log('Token invalid', error);
+            return null;
+        }
     }
 
     static getDecodeRefreshTokenData(token) {
@@ -46,14 +48,14 @@ class Utils {
         return decoded_data;
     }
     static createMaXe(loaiXe) {
-        if(!loaiXe) return null;
-        const date  = new Date().getTime() + Math.floor(Math.random() * 100);
+        if (!loaiXe) return null;
+        const date = new Date().getTime() + Math.floor(Math.random() * 100);
         return loaiXe + date;
     }
     static createMaThueXe(paymentMethod) {
-        if(!paymentMethod) return null;
-        const date  = new Date().getTime();
-        switch(paymentMethod) {
+        if (!paymentMethod) return null;
+        const date = new Date().getTime();
+        switch (paymentMethod) {
             case 'ONLINE':
                 return 'ON' + date;
             case 'OFFLINE':
@@ -64,6 +66,38 @@ class Utils {
                 return null;
         }
     }
+    static sortObject(obj) {
+        let sorted = {};
+        let str = [];
+        let key;
+        for (key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                str.push(encodeURIComponent(key));
+            }
+        }
+        str.sort();
+        for (key = 0; key < str.length; key++) {
+            sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, "+");
+        }
+        return sorted;
+    }
+    static getVnpUrl(vnp_Params, vnp_SecretKey, vnpUrl) {
+        vnp_Params = this.sortObject(vnp_Params);
+        let signData = QueryString.stringify(vnp_Params, { encode: false });
+        let hmac = crypto.createHmac("sha512", vnp_SecretKey);
+        let signed = hmac.update(new Buffer(signData, 'utf-8')).digest("hex");
+        vnp_Params['vnp_SecureHash'] = signed;
+        vnpUrl += '?' + QueryString.stringify(vnp_Params, { encode: false });
+        return vnpUrl;
+    }
+    static getCheckSum(vnp_Params, secretKey) {
+        vnp_Params = this.sortObject(vnp_Params);
+        let signData = QueryString.stringify(vnp_Params, { encode: false });
+        let hmac = crypto.createHmac("sha512", secretKey);
+        let signed = hmac.update(new Buffer(signData, 'utf-8')).digest("hex");
+        return signed;
+    }
 }
+
 
 export default Utils;
