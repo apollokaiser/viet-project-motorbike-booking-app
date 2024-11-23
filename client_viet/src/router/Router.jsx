3cdf0d9d369259f7b12edbce98bf84b02712d7ea
thinh <thinh.ngohoai@gmail.com>
2024-11-23 09:39:31 +0700

    ADD payment notification routes

--- client_viet/src/router/Router.jsx
+++ client_viet/src/router/Router.jsx
@@ -5,43 +5,64 @@ import Page from "../layouts/Page.jsx";
 import Home from "../layouts/Home.jsx";
 import SearchResult from "../layouts/SearchResult.jsx";
 import Cart from "@comps/cart/Cart.jsx";
-
+import { getXe } from "@/apis/getData.js";
+import OrderStatus from "@/layouts/OrderStatus.jsx";
+import OrderSuccess from "@comps/payment/OrderSuccess.jsx";
+import OrderFail from "@comps/payment/OrderFail.jsx";
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Page />,
-      children: [
-        {
-          path: "",
-          element: <Home />,
-          index: true,
+  {
+    path: "/",
+    element: <Page />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+        index: true,
+      },
+      {
+        path: "/add-bikes",
+        element: <AddBikes />,
+      },
+      {
+        path: "/chi-tiet-xe/:id",
+        element: <ProductDetail />,
+        loader: async ({ params }) => {
+          const result = await getXe(params.id);
+          return result.data;
         },
-        {
-          path: "/add-bikes",
-          element: <AddBikes />,
-        },
-        {
-          path: "/chi-tiet-xe/:id",
-          element: <ProductDetail />,
-        },
-        {
-          path:'/tim-kiem',
-          element: <SearchResult/>
-        },
-        {
-          path:"/gio-hang",
-          element:<Cart/>
-        }
-      ],
-    },
-  ]);
-function Router({children}) {
-    return (<>
-        <RouterProvider router={router}>
-            {children}
-        </RouterProvider>
-    </>)
+      },
+      {
+        path: "/tim-kiem",
+        element: <SearchResult />,
+      },
+      {
+        path: "/gio-hang",
+        element: <Cart />,
+      },
+    ],
+  },
+  {
+    path: "thanh-toan/",
+    element: <OrderStatus />,
+    children: [
+      {
+        path: "thanh-cong",
+        element: <OrderSuccess />,
+      },
+      {
+        path:"that-bai",
+        element: <OrderFail />
+      }
+    ],
+  },
+]);
+function Router({ children }) {
+  return (
+    <>
+      <RouterProvider router={router}>{children}</RouterProvider>
+    </>
+  );
 }
 
 export default Router;
