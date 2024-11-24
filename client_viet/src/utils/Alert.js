e9cf02df8a8c6d9a98b05c30744da8774c1889e4
thinh <thinh.ngohoai@gmail.com>
2024-11-24 22:42:02 +0700

    ADD: class Alert

--- client_viet/src/utils/Alert.js
+++ client_viet/src/utils/Alert.js
@@ -1,6 +1,4 @@
-import Swal from "sweetalert2";
-
-
+import Swal, { SweetAlertIcon } from "sweetalert2";
 const toast = Swal.mixin({
     toast: true,
     position: 'top-end',
@@ -12,4 +10,41 @@ const toast = Swal.mixin({
         toast.addEventListener('mouseleave', Swal.resumeTimer);
     }
 });
+export default class Alert {
+    /**
+ * 
+ * @param {string} message 
+ * @param {SweetAlertIcon} icon 
+ */
+    static showToast = (message, icon) => {
+        toast.fire({
+            icon: icon,
+            title: message,
+        });
+    }
+    static showError = (message) => {
+        this.showToast(message, 'error');
+    }
+    static showSuccess = (message) => {
+        this.showToast(message, 'success');
+    }
+    /**
+     * @param {string} message 
+     * @param {SweetAlertIcon} icon 
+     * @param {Function} cb 
+     */
+    static showAlertDialog = (title, message, icon = "success", cb = null, buttonText = "Trở lại") => {
+        (async () => {
+            const { value: confirm } = await Swal.fire({
+                title: title,
+                text: message,
+                icon: icon,
+                confirmButtonText: buttonText
+            });
+            if (confirm && cb) {
+                cb(confirm)
+            }
+        })();
+    }
+}
 export { toast }
