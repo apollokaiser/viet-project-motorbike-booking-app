7fa84443ad420e298bdd03eac7a3a5bf6f0c42e2
thinh <thinh.ngohoai@gmail.com>
2024-11-24 22:46:18 +0700

    ADD: get user info

--- QLthuexeBackend/src/service/khachhang.service.js
+++ QLthuexeBackend/src/service/khachhang.service.js
@@ -1,5 +1,7 @@
 
 import khachHang from "../models/khachhang.js";
+import { ResponseBody, ResponseMessage } from "./payload/ResponseMessage.js"
+import utils from "../utils/utils.js";
 class KhachHangService {
     static async getALLKhachhang(req, res) {
         try {
@@ -29,45 +31,49 @@ class KhachHangService {
             })
         }
     }
+    static async getUserInfo(req, res) {
+        try {
+            const { id } = req.query;
+            const user = await khachHang.findOne({
+                where: { google_id: id }
+            })
+            if (!user) {
+                return res.status(200).send(new ResponseMessage("User not found", 404));
+            }
+            return res.status(200).send(new ResponseBody("User info", user, 200));
+        } catch (error) {
+            console.log("Error get user info : " + error);
+            return res.status(200).send(new ResponseMessage("Error getting user info", 500));
+        }
+    }
     static async updateCustomerInfo(req, res) {
         try {
-            const { customer } = req.body;
-            //Kiểm tra đăng nhập
-            const id = 1;            //Kiểm tra đăng nhập (1 là để test)
-            const result = await khachHang.update(
+            const { userInfo } = req.body;
+            const user = {
+                CMND: userInfo.CMND.id,
+                GPLX: userInfo.GPLX.id,
+                ho_ten: userInfo.CMND.name,
+                sdt: userInfo.phone,
+                GPLX_type: userInfo.GPLX.class
+            }
+            const result = await khachHang.update(user,
                 {
-                    CMND: customer.CMND,
-                    GPLX: customer.GPLX,
-                    ho_ten: customer.ho_ten,
-                    email: customer.email,
-                    sdt: customer.sdt
-                },
-                {
-                    where: {
-                        google_id: id
-                    }
+                    where: { google_id: req.user.google_id }
                 }
             );
             if (!result) {
-                return res.status(200).send({
-                    status: 401,
-                    data: null,
-                    message: 'create khach hang user fail'
-                });
+                return res.status(200).send(new ResponseMessage("cannot update", 400));
             }
-            return res.status(200).send({
-                status: 200,
-                message: 'success',
-                data: createKH
-            });
-
+            delete user.GPLX_type;
+            delete user.sdt;
+            user.google_id = req.user.google_id;
+            user.email = req.user.email;
+            const jwt = utils.createJWT(user);
+            const refreshToken = utils.createRefreshToken(user);
+            return res.status(200).send(new ResponseBody("update successfully", { jwt, refreshToken, user }, 200));
         } catch (err) {
             console.log(err);
-            return res.status(200).send({
-                status: 401,
-                message: 'create khach hang fail',
-                data: null
-            });
+            return res.status(200).send(new ResponseMessage("update failed", 500));
         }
     }
 }
