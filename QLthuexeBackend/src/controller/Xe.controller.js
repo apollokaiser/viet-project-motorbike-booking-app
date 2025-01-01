2300afa36b32de2d0c598da40213020f28df6db5
thinh <thinh.ngohoai@gmail.com>
2025-01-01 22:26:54 +0700

    add enpoints

--- QLthuexeBackend/src/controller/Xe.controller.js
+++ QLthuexeBackend/src/controller/Xe.controller.js
@@ -6,11 +6,16 @@ const router = express.Router();
 router.get("/load-bike-status", XeService.getBikeStatus)
 router.get("/get-all-bikes-data", XeService.getAllXeData)
 router.delete("/delete-bike", XeService.deleteXe)
-router.get("/active-bike", XeService.activeBike)
+router.get("/active-bike", XeService.activeXe)
+router.put("/cap-nhat-xe", XeService.updateXe)
+router.get("/get-bien-so-xe", XeService.getAllBienSoXeByXeId)
+router.delete("/xoa-bien-so-xe", XeService.deleteBienSoXe)
+router.get("/active-bien-so-xe", XeService.activeBienSoXe)
+router.post("/them-bien-so-xe", XeService.addBienSoXe)
 
 // http://localhost:8080/api/xe/*
 router.get('/get-xe', XeService.getXe)
-router.get('/related-products', XeService.getRelatedProducts)
+router.get('/related-products', XeService.getRelatedBikes)
 router.get('/tim-kiem-xe', XeService.searchXe)
 router.post('/add-xe', XeService.addXe)
 router.get('', XeService.getALLXe)
