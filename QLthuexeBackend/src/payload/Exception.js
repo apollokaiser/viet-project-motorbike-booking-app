c83ea9f8e1bf9f7d43133348aeb4d0291a7097aa
thinh <thinh.ngohoai@gmail.com>
2025-01-01 22:24:55 +0700

    move payload out of service and add exception class

---
+++ QLthuexeBackend/src/payload/Exception.js
@@ -0,0 +1,21 @@
+import { ResponseMessage } from "./ResponseMessage";
+
+export default class Exception {
+    constructor(message, code = 400) {
+        this.message = message;
+        this.code = code;
+    }
+    /**
+     * 
+     * @param {Error} error 
+     * @param {String} defaultMessage 
+     * @param {Number} statusCode 
+     * @returns Return an error payload for response 
+     */
+    static sendError(error, defaultMessage = "error", statusCode = 400) {
+        if (error instanceof Exception) {
+            return new ResponseMessage((error.message, error.code));
+        }
+        return new ResponseMessage(defaultMessage, statusCode);
+    }
+}
