47ff7f50c79d804f2e307da3333e6dde7f84eb7d
thinh <thinh.ngohoai@gmail.com>
2025-01-13 11:00:31 +0700

    add order history and report endpoint

--- client_viet/src/router/Router.jsx
+++ client_viet/src/router/Router.jsx
@@ -1,33 +1,41 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 //Admin
-import AdminLoginPage from "@/pages/admin/Login/AdminLoginPage.jsx";
-import AdminHome from "../pages/admin/Home/Home.jsx";
+import ProductContent from "@/features/admin/vehicle/components/ProductContent.jsx";
+import UpdateProductPage from "@/features/admin/vehicle/UpdateProductPage.jsx";
+import RentalCompletion from "@/features/admin/order/RentalCompletion.jsx";
+import AdminLoginPage from "@/features/admin/login/AdminLoginPage.jsx";
+import ProductPage from "@/features/admin/vehicle/ProductPage.jsx";
+import AdminHome from "@/features/admin/home/Home.jsx";
 //Web (user)
-import ProductDetail from "../pages/web/Product/ProductDetailPage.jsx";
-import SearchPage from "../pages/web/Search/SearchResult.jsx";
-import OrderSuccess from "@comps/payment/OrderSuccess.jsx";
-import OrderStatus from "@pages/web/Order/OrderStatus.jsx";
-import UserPage from "@pages/web/Profile/UserPage.jsx";
-import OrderFail from "@comps/payment/OrderFail.jsx";
-import HomePage from "../pages/web/Home/HomePage.jsx";
+import ProductDetailPage from "@/features/web/vehicle/ProductDetailPage.jsx";
+import OrderSuccess from "@/features/web/payment/OrderSuccess.jsx";
+import OrderStatus from "@/features/web/payment/PaymentStatus.jsx";
+import SearchPage from "../features/web/search/SearchResult.jsx";
+import OrderFail from "@/features/web/payment/OrderFail.jsx";
+import HomePage from "../features/web/home/HomePage.jsx";
+import UserPage from "@/features/web/user/UserPage.jsx";
+import CartPage from "@/features/web/cart/CartPage.jsx";
+//Error(for all)
+import Unauthorized from "@/features/error/Unauthorized.jsx";
+import NotFound from "@/features/error/NotFound.jsx";
 //layouts
-import AdminPage from "@/layouts/admin/Page.jsx";
-import Page from "@/layouts/web/Page.jsx";
+import AdminPage from "@/features/admin/layout/Page.jsx";
+import WebPage from "@/features/web/layout/Page.jsx";
 
-import ProductPage from "@pages/admin/Product/ProductPage.jsx";
-import ProductContent from "@comps/admin/product/ProductContent.jsx";
 import ProductService from "@/services/ProductService.js";
-import NotFound from "@pages/error/NotFound.jsx";
-import Unauthorized from "@pages/error/Unauthorized.jsx";
 import JWTService from "@/services/JWTService.js";
-import CartPage from "@pages/web/Cart/CartPage.jsx";
-import UpdateProductPage from "@pages/admin/Product/UpdateProductPage.jsx";
+import ReportPage from "@/features/admin/report/ReportPage.jsx";
+import UserInfo from "@/features/web/user/UserInfo.jsx";
+import OrderHistory from "@/features/web/user/OrderHistory.jsx";
+import OrderHistoryContent from "@/features/web/user/components/OrderHistoryContent.jsx";
+import OrderInfomation from "@/features/web/user/OrderInfomation.jsx";
+import ProductListPage from "@/features/web/vehicle/ProductListPage.jsx";
 
 const router = createBrowserRouter([
   {
     path: "",
-    element: <Page />,
+    element: <WebPage />,
     children: [
       {
         path: "",
@@ -36,7 +44,7 @@ const router = createBrowserRouter([
       },
       {
         path: "/chi-tiet-xe/:id",
-        element: <ProductDetail />,
+        element: <ProductDetailPage />,
         loader: async ({ params }) => {
           const result = await ProductService.getBike(params.id);
           return result.data;
@@ -47,13 +55,39 @@ const router = createBrowserRouter([
         element: <SearchPage />,
       },
       {
+        path:"/danh-sach-xe",
+        element:<ProductListPage />
+      },
+      {
         path: "/gio-hang",
         element: <CartPage />,
       },
       {
-        path:"/thong-tin-khach-hang",
-        element:<UserPage />
-      }
+        path: "/khach-hang",
+        element: <UserPage />,
+        children:[
+          {
+            path: "thong-tin",
+            element: <UserInfo />,
+            index: true
+          },
+          {
+            path: "lich-su-thue-xe",
+            element:<OrderHistory />,
+            children:[
+              {
+                path:"",
+                element:<OrderHistoryContent />,
+                index:true
+              },
+              {
+                path:"chi-tiet",
+                element: <OrderInfomation />
+              }
+            ]
+          }
+        ]
+      },
     ],
   },
   {
@@ -65,55 +99,64 @@ const router = createBrowserRouter([
         element: <OrderSuccess />,
       },
       {
-        path:"that-bai",
-        element: <OrderFail />
-      }
+        path: "that-bai",
+        element: <OrderFail />,
+      },
     ],
-  }, {
-    path:"login/admin",
-    element:<AdminLoginPage />,
-    loader: () =>{
+  },
+  {
+    path: "login/admin",
+    element: <AdminLoginPage />,
+    loader: () => {
       const admin = JWTService.decode(localStorage.getItem("jwt_admin"));
       if (admin) {
         return <AdminHome />;
       } else {
         return <AdminLoginPage />;
       }
-    }
+    },
   },
   {
     path: "/admin",
     element: <AdminPage />,
-    children:[
+    children: [
       {
-        path:"",
-        element:<AdminHome />
+        path: "",
+        element: <AdminHome />,
       },
       {
-        path:"product",
-        element:<ProductPage />,
-        children:[
+        path:"xuat-hoa-don",
+        element: <RentalCompletion />,
+      },
+      {
+        path: "product",
+        element: <ProductPage />,
+        children: [
           {
-            path:"",
-            element:<ProductContent />,
+            path: "",
+            element: <ProductContent />,
             index: true,
           },
           {
-            path:"cap-nhat-xe",
-            element:<UpdateProductPage />
-          }
-        ]
+            path: "cap-nhat-xe",
+            element: <UpdateProductPage />,
+          },
+        ],
+      },
+      {
+        path:"bao-cao",
+        element: <ReportPage />,
       }
-    ]
+    ],
   },
   {
     path: "/unauthorized",
-    element: <Unauthorized />
+    element: <Unauthorized />,
   },
   {
     path: "*",
-    element: <NotFound />
-  }
+    element: <NotFound />,
+  },
 ]);
 function Router({ children }) {
   return (
