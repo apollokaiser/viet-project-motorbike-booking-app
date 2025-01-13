eef70244b9cf03f0419a1f9faeefc1217d1ba820
thinh <thinh.ngohoai@gmail.com>
2025-01-13 11:05:23 +0700

    add fines and order status datas

--- QLthuexeBackend/src/utils/initialize.js
+++ QLthuexeBackend/src/utils/initialize.js
@@ -2,6 +2,9 @@ import hangXe from "../models/hangxe.js";
 import loaiXe from "../models/loaixe.js";
 import vanChuyen from "../models/vanchuyen.js";
 import nhanVien from "../models/nhanvien.js"
+import phiPhat from "../models/phiphat.js";
+import phuongThucThanhToan from "../models/phuongthucthanhtoan.js";
+import tinhTrangThue from "../models/tinhtrangthue.js";
 const brandsData = [
     {
         ma_hang: "HONDA",
@@ -37,7 +40,7 @@ const phiVanChuyenData = [
         to: 5,
         phi_giao_xe: 20000
     }
-    ,{
+    , {
         from: 5,
         to: 10,
         phi_giao_xe: 30000
@@ -54,13 +57,69 @@ const admin = {
     email: "admin@example.com",
     mat_khau: "admin123",
 }
+const phiPhats = [
+    {
+        ma_phi_phat: 0,
+        ly_do_phat: "Trễ hạn",
+        tien_phat: 200000
+    },
+    {
+        ma_phi_phat: 1,
+        ly_do_phat: "Làm hư xe",
+        tien_phat: 100000
+    },
+    {
+        ma_phi_phat: 2,
+        ly_do_phat: "Khác",
+        tien_phat: 300000
+    }
+]
+const ptThanhToanDatas = [
+    {
+        ma_thanh_toan: "ONLINE",
+        ten_thanh_toan: "Thanh toán bằng chuyển khoản"
+    },
+    {
+        ma_thanh_toan: "OFFLINE",
+        ten_thanh_toan: "Thanh toán trực tiếp tại hãng"
+    }, {
+        ma_thanh_toan: "COD",
+        ten_thanh_toan: "Thanh toán tại địa chỉ giao xe"
+    }
+]
+export const orderStatus = [
+    {
+        ma_tinh_trang: 0,
+        ten_tinh_trang: 'Chờ thanh toán'
+    },
+    {
+        ma_tinh_trang: 1,
+        ten_tinh_trang: 'Chờ xác nhận'
+    },
+    {
+        ma_tinh_trang: 2,
+        ten_tinh_trang: 'Đã xác nhận'
+    },
+    {
+        ma_tinh_trang: 3,
+        ten_tinh_trang: 'Đã giao'
+    },
+    {
+        ma_tinh_trang: 4,
+        ten_tinh_trang: 'Đã hủy'
+    },
+    {
+        ma_tinh_trang: 5,
+        ten_tinh_trang: 'Đã hoàn thành'
+    }
+]
 
 export const addLoaiXes = async () => {
     try {
         const count = await loaiXe.count();
         if (count == 0) {
             loaiXe.bulkCreate(categoriesData);
-            
+
         }
     } catch (error) {
         console.log("Initialize - CATEGORY failed: " + error);
@@ -96,31 +155,37 @@ export const addAdminAccount = async () => {
         console.log("Initialize - ADMIN ACCOUNT failed: " + error)
     }
 }
-
-
-export const orderStatus = {
-    PENDING_PAYMENT: {
-        id: 0,
-        name: 'Chờ thanh toán'
-    },
-    WAITING_CONFIRMATION: {
-        id: 1,
-        name: 'Chờ xác nhận'
-    },
-    CONFIRMED: {
-        id: 2,
-        name: 'Đã xác nhận'
-    },
-    DELIVERED: {
-        id: 3,
-        name: 'Đã giao'
-    },
-    RETURNED: {
-        id: 4,
-        name: 'Đã trả'
-    },
-    CANCELLED: {
-        id: 5,
-        name: 'Đã hủy'
+export const addPhiPhat = async () => {
+    try {
+        const count = await phiPhat.count();
+        if (count == 0) {
+            phiPhat.bulkCreate(phiPhats);
+        }
+    } catch (error) {
+        console.log("Initialize - PHI PHAT failed: " + error)
     }
 }
+export const addPTThanhtoan = async () => {
+    try {
+        const count = await phuongThucThanhToan.count();
+        if (count == 0) {
+            phuongThucThanhToan.bulkCreate(ptThanhToanDatas);
+        }
+    } catch (error) {
+        console.log("Initialize - PHUONG THUC THANH TOAN failed: " + error)
+    }
+}
+export const addOrderStatus = async () => {
+    try {
+        const count = await tinhTrangThue.count();
+        if (count == 0) {
+            tinhTrangThue.bulkCreate(orderStatus);
+        }
+    } catch (error) {
+        console.log("Initialize - ORDER STATUS failed: " + error)
+    }
+}
+
+
+
+
