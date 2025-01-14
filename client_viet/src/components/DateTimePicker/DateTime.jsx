61524712b10af65e6e54014ce552c7b82acdc98b
thinh <thinh.ngohoai@gmail.com>
2025-01-14 20:16:13 +0700

    update datetime picker component with no default value

--- client_viet/src/components/DateTimePicker/DateTime.jsx
+++ client_viet/src/components/DateTimePicker/DateTime.jsx
@@ -2,23 +2,31 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import dayjs from "dayjs";
-import { useState } from "react";
 
-function DateTime({ updateDate, label, name, defaultValue}) {
-  const [date, setDate] = useState(new Date());
+function DateTime({
+  updateDate,
+  label,
+  name,
+  defaultValue,
+  format = "DD/MM/YYYY",
+}) {
   const changeDate = (value) => {
-    let dateFormat = dayjs(value).format("DD/MM/YYYY");
-    setDate(dateFormat);
-    updateDate(dayjs(value).unix(), name);
+     updateDate(dayjs(value).unix(), name);
+  };
+  const getDefaultDate = () => {
+    if (!defaultValue) {
+      return dayjs(new Date().getTime());
+    }
+    return dayjs(new Date(defaultValue * 1000));
   };
   return (
     <>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <DatePicker
           label={label}
-          defaultValue={dayjs(new Date(defaultValue * 1000)) || dayjs(date)}
+          defaultValue={getDefaultDate()}
           onChange={changeDate}
-          format={"DD/MM/YYYY"}
+          format={format}
         />
       </LocalizationProvider>
     </>
